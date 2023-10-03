b4d586af34467604cf50474a9c1c1667ca93e5fd
Rojan Rajbhandari <rrojan20@gmail.com>
2023-10-03 09:50:58 +0545

    feat: capture filter-applied photo

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 import cv2
 from utils import get_filter_path, read_image, switch_filter
+from time import time, sleep
 
 # Set default filter as 'glasses'
 filter = 'glasses'
 filter_path = get_filter_path(filter)
 ALL_FILTERS = ['glasses', 'sunglasses', 'coolglasses']
-# Command I used for generating alpha channel image (transparent background):
-#
-# convert glasses.png   -alpha on -background none -flatten glasses-alpha.png
-#
-# NOTE: "transparent" pngs downloaded from the internet can still pose some problems
-# NOTE: so best use thi imagemagick command just in case
-
 
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 sunglasses, sunglasses_height, sunglasses_resized = read_image(filter_path)
 
-
 cap = cv2.VideoCapture(0)
+photo_captured = False
+photo_captured_time = 0
 
 while True:
     # Capture single frame from webcam
@@ -37,7 +32,7 @@ while True:
 
     for (x, y, w, h) in faces:
         # Resize the sunglasses image to match the dimensions of the detected face (ROI)
-        # TODO: Since we're working with glasses we should actually match to the eyes intead of performing full face detectin
+        # TODO: Since we're working with glasses we should actually match to the eyes intead of performing full face detection
         # TODO: and overlaying to the full face.
         sunglasses_resized = cv2.resize(sunglasses, (w, h))
 
@@ -45,7 +40,7 @@ while True:
         # (this is the frame where the sunglasses will be placed)
         roi = frame[y:y+h, x:x+w]
 
-        # Create glasses mask and invert for ares
+        # Create glasses mask and invert for areas
         sunglasses_alpha = sunglasses_resized[:, :, 3]
         sunglasses_area = cv2.bitwise_not(sunglasses_alpha)
         sunglasses_rgb = sunglasses_resized[:, :, :3]
@@ -54,13 +49,28 @@ while True:
         # overlay glasses and ROI together for filter effect!
         frame[y:y+h, x:x+w] = cv2.add(roi, sunglasses_rgb)
 
+    if photo_captured:
+        current_time = time()
+        if current_time - photo_captured_time <= 2:
+            cv2.putText(frame, "Photo Captured", (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
     cv2.imshow('Haarcascade Mayajaal', frame)
 
-    # break on "q" press
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Capture a screenshot when spacebar is pressed
+    key = cv2.waitKey(1)
+    if key == ord(' '):
+        screenshot_name = f'screenshot-{int(time())}.png'
+        cv2.imwrite(screenshot_name, frame)
+        print(f'Screenshot saved as {screenshot_name}')
+        photo_captured = True
+        photo_captured_time = time()
+
+    # Break on "q" press
+    if key == ord('q'):
         break
     # Switch filter on "s" key press
-    elif cv2.waitKey(1) & 0xFF == ord('s'):
+    elif key == ord('s'):
         filter = switch_filter(filter, ALL_FILTERS)
         filter_path = get_filter_path(filter)
         sunglasses, sunglasses_height, sunglasses_resized = read_image(
